8630265067a9e18d39e541ea0a7087c2ab63060b
mayur-solace <mayur.patel@solace.com>
2023-04-03 16:58:44 -0400

    Spring Boot version upgrade 	- Update Spring Boot dependency to latest release version of 3.x 	- Update JDK version required to build to 17   - Update JMS imports from javax.jms to jakarta.jms

--- solace-spring-boot-autoconfigure/solace-jms-spring-boot-autoconfigure/src/main/java/com/solace/spring/boot/autoconfigure/SolaceJmsAutoConfiguration.java
+++ solace-spring-boot-autoconfigure/solace-jms-spring-boot-autoconfigure/src/main/java/com/solace/spring/boot/autoconfigure/SolaceJmsAutoConfiguration.java
@@ -18,7 +18,7 @@
  */
 package com.solace.spring.boot.autoconfigure;
 
-import javax.jms.ConnectionFactory;
+import jakarta.jms.ConnectionFactory;
 
 import com.solace.services.core.loader.SolaceCredentialsLoader;
 import com.solace.services.core.model.SolaceServiceCredentials;
