6aad29bdfcc94393dc5cc1c3162f9fb20d278fa2
mayur-solace <mayur.patel@solace.com>
2023-05-11 16:42:58 -0400

    SOL-94979 Remove CloudFoundry Support

--- solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/main/java/com/solace/spring/boot/autoconfigure/SolaceJavaAutoConfiguration.java
+++ solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/main/java/com/solace/spring/boot/autoconfigure/SolaceJavaAutoConfiguration.java
@@ -18,44 +18,84 @@
  */
 package com.solace.spring.boot.autoconfigure;
 
-import com.solace.services.core.loader.SolaceCredentialsLoader;
-import com.solace.services.core.model.SolaceServiceCredentials;
+import com.solacesystems.jcsmp.JCSMPChannelProperties;
+import com.solacesystems.jcsmp.JCSMPProperties;
+import com.solacesystems.jcsmp.SpringJCSMPFactory;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.solacesystems.jcsmp.JCSMPProperties;
-import com.solacesystems.jcsmp.SpringJCSMPFactory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @AutoConfigureBefore(JmsAutoConfiguration.class)
 @ConditionalOnClass({JCSMPProperties.class})
 @ConditionalOnMissingBean(SpringJCSMPFactory.class)
 @EnableConfigurationProperties(SolaceJavaProperties.class)
-public class SolaceJavaAutoConfiguration extends SolaceJavaAutoConfigurationBase {
-    private SolaceCredentialsLoader solaceServicesInfoLoader = new SolaceCredentialsLoader();
+public class SolaceJavaAutoConfiguration {
 
-    @Autowired
-    public SolaceJavaAutoConfiguration(SolaceJavaProperties properties) {
-        super(properties);
+  private SolaceJavaProperties properties;
+
+  @Autowired
+  public SolaceJavaAutoConfiguration(SolaceJavaProperties properties) {
+    this.properties = properties;
+  }
+
+  /**
+   * Returns a {@link SpringJCSMPFactory} based on {@link JCSMPProperties} bean.
+   *
+   * @return {@link SpringJCSMPFactory} based on {@link JCSMPProperties} bean.
+   */
+  @Bean
+  public SpringJCSMPFactory getSpringJCSMPFactory(JCSMPProperties jcsmpProperties) {
+    return new SpringJCSMPFactory(jcsmpProperties);
+  }
+
+  /**
+   * Returns a {@link JCSMPProperties} based on {@link SolaceJavaProperties}.
+   *
+   * @return {@link JCSMPProperties} based on {@link SolaceJavaProperties}.
+   */
+  @Bean
+  public JCSMPProperties getJCSMPProperties() {
+    Properties p = new Properties();
+    Set<Entry<String, String>> set = properties.getApiProperties().entrySet();
+    for (Map.Entry<String, String> entry : set) {
+      p.put("jcsmp." + entry.getKey(), entry.getValue());
     }
 
-    @Override
-    SolaceServiceCredentials findFirstSolaceServiceCredentialsImpl() {
-        return solaceServicesInfoLoader.getSolaceServiceInfo();
+    JCSMPProperties jcsmpProps = createFromApiProperties(p);
+    jcsmpProps.setProperty(JCSMPProperties.HOST, properties.getHost());
+    jcsmpProps.setProperty(JCSMPProperties.VPN_NAME, properties.getMsgVpn());
+    jcsmpProps.setProperty(JCSMPProperties.USERNAME, properties.getClientUsername());
+    jcsmpProps.setProperty(JCSMPProperties.PASSWORD, properties.getClientPassword());
+    if ((properties.getClientName() != null) && (!properties.getClientName().isEmpty())) {
+      jcsmpProps.setProperty(JCSMPProperties.CLIENT_NAME, properties.getClientName());
     }
 
-    @Override
-    public List<SolaceServiceCredentials> getSolaceServiceCredentials() {
-        return new ArrayList<>(solaceServicesInfoLoader.getAllSolaceServiceInfo().values());
-    }
+    // Channel Properties
+    JCSMPChannelProperties cp = (JCSMPChannelProperties) jcsmpProps
+        .getProperty(JCSMPProperties.CLIENT_CHANNEL_PROPERTIES);
+    cp.setConnectRetries(properties.getConnectRetries());
+    cp.setReconnectRetries(properties.getReconnectRetries());
+    cp.setConnectRetriesPerHost(properties.getConnectRetriesPerHost());
+    cp.setReconnectRetryWaitInMillis(properties.getReconnectRetryWaitInMillis());
+    return jcsmpProps;
+  }
 
+  private JCSMPProperties createFromApiProperties(Properties apiProps) {
+    return apiProps != null ? JCSMPProperties.fromProperties(apiProps) : new JCSMPProperties();
+  }
+
+
+  void setProperties(SolaceJavaProperties properties) {
+    this.properties = properties;
+  }
 }
--- solace-spring-boot-autoconfigure/solace-jms-spring-boot-autoconfigure/src/main/java/com/solace/spring/boot/autoconfigure/SolaceJmsAutoConfiguration.java
+++ solace-spring-boot-autoconfigure/solace-jms-spring-boot-autoconfigure/src/main/java/com/solace/spring/boot/autoconfigure/SolaceJmsAutoConfiguration.java
@@ -18,45 +18,98 @@
  */
 package com.solace.spring.boot.autoconfigure;
 
+import com.solacesystems.jms.SolConnectionFactory;
+import com.solacesystems.jms.SolConnectionFactoryImpl;
+import com.solacesystems.jms.property.JMSProperties;
 import jakarta.jms.ConnectionFactory;
-
-import com.solace.services.core.loader.SolaceCredentialsLoader;
-import com.solace.services.core.model.SolaceServiceCredentials;
+import java.util.Hashtable;
+import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jndi.JndiTemplate;
 
-import com.solacesystems.jms.SolConnectionFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
 @AutoConfigureBefore(JmsAutoConfiguration.class)
-@ConditionalOnClass({ ConnectionFactory.class, SolConnectionFactory.class })
-@ConditionalOnMissingBean({ ConnectionFactory.class, JndiTemplate.class })
+@ConditionalOnClass({ConnectionFactory.class, SolConnectionFactory.class})
+@ConditionalOnMissingBean({ConnectionFactory.class, JndiTemplate.class})
 @EnableConfigurationProperties(SolaceJmsProperties.class)
-public class SolaceJmsAutoConfiguration extends SolaceJmsAutoConfigurationBase {
+public class SolaceJmsAutoConfiguration {
 
-    private SolaceCredentialsLoader solaceServicesInfoLoader = new SolaceCredentialsLoader();
+  private static final Logger logger = LoggerFactory.getLogger(
+      SolaceJmsAutoConfiguration.class);
 
-    @Autowired
-    public SolaceJmsAutoConfiguration(SolaceJmsProperties properties) {
-        super(properties);
+  private SolaceJmsProperties properties;
+
+  @Autowired
+  public SolaceJmsAutoConfiguration(SolaceJmsProperties properties) {
+    this.properties = properties;
+  }
+
+  /**
+   * Returns a {@link SolConnectionFactory} based on the {@link SolaceJmsProperties}.
+   *
+   * @return {@link SolConnectionFactory} based on the {@link SolaceJmsProperties}
+   */
+  @Bean
+  public SolConnectionFactory getSolConnectionFactory() {
+    try {
+      Hashtable<String, String> ht = new Hashtable<>(properties.getApiProperties());
+      JMSProperties props = new JMSProperties(ht);
+      props.initialize();
+      SolConnectionFactoryImpl cf = new SolConnectionFactoryImpl(props);
+      cf.setHost(properties.getHost());
+      cf.setVPN(properties.getMsgVpn());
+      cf.setUsername(properties.getClientUsername());
+      cf.setPassword(properties.getClientPassword());
+      cf.setDirectTransport(properties.isDirectTransport());
+      return cf;
+    } catch (Exception ex) {
+      logger.error("Exception found during Solace Connection Factory creation.", ex);
+      throw new IllegalStateException("Unable to create Solace connection factory, "
+          + "ensure that the sol-jms-jakarta-<version>.jar "
+          + "is the classpath", ex);
     }
+  }
 
-    @Override
-    SolaceServiceCredentials findFirstSolaceServiceCredentialsImpl() {
-        return solaceServicesInfoLoader.getSolaceServiceInfo();
-    }
+  /**
+   * Returns a {@link JndiTemplate} based on the {@link SolaceJmsProperties}.
+   *
+   * @return {@link JndiTemplate} based on the {@link SolaceJmsProperties}
+   */
+  @Bean
+  public JndiTemplate getJndiTemplate() {
+    try {
+      Properties env = new Properties();
+      env.putAll(properties.getApiProperties());
+      env.put(InitialContext.INITIAL_CONTEXT_FACTORY,
+          "com.solacesystems.jndi.SolJNDIInitialContextFactory");
+      env.put(InitialContext.PROVIDER_URL, properties.getHost());
+      env.put(Context.SECURITY_PRINCIPAL,
+          properties.getClientUsername() + '@' + properties.getMsgVpn());
+      env.put(Context.SECURITY_CREDENTIALS, properties.getClientPassword());
 
-    @Override
-    public List<SolaceServiceCredentials> getSolaceServiceCredentials() {
-        return new ArrayList<>(solaceServicesInfoLoader.getAllSolaceServiceInfo().values());
+      JndiTemplate jndiTemplate = new JndiTemplate();
+      jndiTemplate.setEnvironment(env);
+      return jndiTemplate;
+    } catch (Exception ex) {
+      logger.error("Exception found during Solace JNDI Initial Context creation.", ex);
+      throw new IllegalStateException("Unable to create Solace "
+          + "JNDI Initial Context, ensure that the sol-jms-jakarta-<version>.jar "
+          + "is the classpath", ex);
     }
+  }
+
+  void setProperties(SolaceJmsProperties properties) {
+    this.properties = properties;
+  }
 }
