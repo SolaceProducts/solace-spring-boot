438a5696c65dac2abf899b617f82db9c7065d396
mayur-solace <mayur.patel@solace.com>
2024-07-26 14:44:25 -0400

    DATAGO-81464: OAuth2 conditional auto configuration activation issue fix

--- solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/test/java/com/solace/spring/boot/autoconfigure/SolaceOAuthClientConfigurationTest.java
+++ solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/test/java/com/solace/spring/boot/autoconfigure/SolaceOAuthClientConfigurationTest.java
@@ -1,10 +1,8 @@
 package com.solace.spring.boot.autoconfigure;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import com.solacesystems.jcsmp.SolaceSessionOAuth2TokenProvider;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -42,22 +40,6 @@ class SolaceOAuthClientConfigurationTest {
       assertThat(context.getBean(SolaceSessionOAuth2TokenProvider.class)).isNotNull();
       assertThat(
           context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class)).isNotNull();
-    }
-  }
-
-  @Test
-  void verifyApplicationContextDoesNotContainOAuth2BeansWhenAuthSchemeIsNotOAuth2() {
-    try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
-        .profiles("oauthConfigIT").sources(TestApp.class)
-        .properties(String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME",
-            "AUTHENTICATION_SCHEME_BASIC"))
-        .run()) {
-      assertThat(context.isRunning()).isTrue();
-
-      assertThatThrownBy(() -> context.getBean(SolaceSessionOAuth2TokenProvider.class))
-          .isInstanceOf(NoSuchBeanDefinitionException.class);
-      assertThatThrownBy(() -> context.getBean(AuthorizedClientServiceOAuth2AuthorizedClientManager.class))
-          .isInstanceOf(NoSuchBeanDefinitionException.class);
     }
   }
 }
