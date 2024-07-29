b3d4aa28a39ed689f9c00575766515995fbf438d
Jeffrey D <11084623+Nephery@users.noreply.github.com>
2024-07-29 10:30:09 -0400

    use random ports for spring app builder tests (#139)

--- solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/test/java/com/solace/spring/boot/autoconfigure/SolaceOAuthClientConfigurationTest.java
+++ solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/test/java/com/solace/spring/boot/autoconfigure/SolaceOAuthClientConfigurationTest.java
@@ -1,7 +1,5 @@
 package com.solace.spring.boot.autoconfigure;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import com.solacesystems.jcsmp.SolaceSessionOAuth2TokenProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
@@ -9,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class SolaceOAuthClientConfigurationTest {
 
@@ -23,7 +24,8 @@ class SolaceOAuthClientConfigurationTest {
         .profiles("oauthConfigIT").sources(TestApp.class)
         .properties(
             String.format("%s=%s", "solace.java.api-properties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"),
-            String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"))
+            String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"),
+            "server.port=0")
         .run()) {
       assertThat(context.isRunning()).isTrue();
       assertThat(context.getBean(SolaceSessionOAuth2TokenProvider.class)).isNotNull();
@@ -38,7 +40,8 @@ class SolaceOAuthClientConfigurationTest {
         .profiles("oauthConfigIT").sources(TestApp.class)
         .properties(
             String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"),
-            String.format("%s=%s", "solace.java.oauth2-client-registration-id", "my-oauth2-client"))
+            String.format("%s=%s", "solace.java.oauth2-client-registration-id", "my-oauth2-client"),
+            "server.port=0")
         .run()) {
       assertThat(context.isRunning()).isTrue();
       assertThat(context.getBean(SolaceSessionOAuth2TokenProvider.class)).isNotNull();
@@ -53,7 +56,8 @@ class SolaceOAuthClientConfigurationTest {
         .profiles("oauthConfigIT").sources(TestApp.class)
         .properties(
             String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_BASIC"),
-            String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"))
+            String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"),
+            "server.port=0")
         .run()) {
       assertThat(context.isRunning()).isTrue();
 
@@ -69,7 +73,8 @@ class SolaceOAuthClientConfigurationTest {
   void verifyApplicationContextDoesNotContainOAuth2BeansWhenAuthSchemePropertyNotDefined() {
     try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
         .profiles("oauthConfigIT").sources(TestApp.class)
-        .properties(String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"))
+        .properties(String.format("%s=%s", "solace.java.oauth2ClientRegistrationId", "my-oauth2-client"),
+                "server.port=0")
         .run()) {
       assertThat(context.isRunning()).isTrue();
 
@@ -85,7 +90,8 @@ class SolaceOAuthClientConfigurationTest {
   void verifyApplicationContextDoesNotContainOAuth2BeansWhenClientRegistrationIdNotDefined() {
     try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
         .profiles("oauthConfigIT").sources(TestApp.class)
-        .properties(String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"))
+        .properties(String.format("%s=%s", "solace.java.apiProperties.AUTHENTICATION_SCHEME", "AUTHENTICATION_SCHEME_OAUTH2"),
+                "server.port=0")
         .run()) {
       assertThat(context.isRunning()).isTrue();
 
