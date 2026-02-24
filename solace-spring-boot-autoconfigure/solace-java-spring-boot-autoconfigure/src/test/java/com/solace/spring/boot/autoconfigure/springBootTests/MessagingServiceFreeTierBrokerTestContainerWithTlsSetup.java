43c0e813d5c2f05c2db2970c7cfbcef6277a4ca6
mayur-solace <mayur.patel@solace.com>
2026-02-24 12:27:29 -0500

    Upgrade spring Boot to v4.0.3, update test dependency versions

--- solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/test/java/com/solace/spring/boot/autoconfigure/springBootTests/MessagingServiceFreeTierBrokerTestContainerWithTlsSetup.java
+++ solace-spring-boot-autoconfigure/solace-java-spring-boot-autoconfigure/src/test/java/com/solace/spring/boot/autoconfigure/springBootTests/MessagingServiceFreeTierBrokerTestContainerWithTlsSetup.java
@@ -21,7 +21,7 @@ public interface MessagingServiceFreeTierBrokerTestContainerWithTlsSetup {
       MessagingServiceFreeTierBrokerTestContainerWithTlsSetup.class);
 
   ComposeContainer COMPOSE_CONTAINER = new ComposeContainer(
-      new File(FULL_DOCKER_COMPOSE_FILE_PATH)).withLocalCompose(true).withPull(true)
+      new File(FULL_DOCKER_COMPOSE_FILE_PATH)).withPull(true)
       .withExposedService(PUBSUB_BROKER_SERVICE_NAME, 8080)
       .withExposedService(PUBSUB_BROKER_SERVICE_NAME, 55443)
       .withExposedService(PUBSUB_BROKER_SERVICE_NAME, 55555)
